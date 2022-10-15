129ca26f31092ef910a13f8bceeb9af07bfe1400
dweisel <drewweisel@gmail.com>
2022-10-15 12:53:28 -0600

    edits

--- scripts.js
+++
@@ -1,2 +0,0 @@
-//* 1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
-// 1a. Programmatically subtract the value of the first element in the array from the value in the last element of the array 
